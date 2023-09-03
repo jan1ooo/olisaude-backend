f4b601d1208b71380a6629ac932c6b12c6c126fc
Janio Filho <filhojanio67@gmail.com>
2023-09-03 13:58:20 -0300

    First commit

---
+++ dev/src/main/java/br/com/jan1ooo/apiolisaude/ApiOlisaudeApplication.java
@@ -0,0 +1,13 @@
+package br.com.jan1ooo.apiolisaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiOlisaudeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiOlisaudeApplication.class, args);
+    }
+
+}
---
+++ dev/src/test/java/br/com/jan1ooo/apiolisaude/ApiOlisaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jan1ooo.apiolisaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiOlisaudeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
